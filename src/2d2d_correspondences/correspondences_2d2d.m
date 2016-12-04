e34364a7c93dd919d14166c66a3994741318b0c0
Toni <tonirosinol@hotmail.com>
2016-12-04 21:28:58 +0100

    Add switch to have the option to debug with figures

--- src/2d2d_correspondences/correspondences_2d2d.m
+++ src/2d2d_correspondences/correspondences_2d2d.m
@@ -1,5 +1,7 @@
 function [keypoints1, keypoints2] = correspondences_2d2d(img1, img2)
 
+debug_with_figures = false;
+
 % Randomly chosen parameters that seem to work well - can you find better
 % ones?
 harris_patch_size = 9;
@@ -15,37 +17,45 @@ img_1 = img1;
 
 harris_scores = harris(img_1, harris_patch_size, harris_kappa);
 assert(min(size(harris_scores) == size(img_1)));
-figure(1);
-subplot(2, 1, 1);
-imshow(img_1);
-subplot(2, 1, 2);
-imagesc(harris_scores);
-axis equal;
-axis off;
+
+if (debug_with_figures)
+    figure(1);
+    subplot(2, 1, 1);
+    imshow(img_1);
+    subplot(2, 1, 2);
+    imagesc(harris_scores);
+    axis equal;
+    axis off;
+end
 
 %% Part 2 - Select keypoints
 
 keypoints_1 = selectKeypoints(...
     harris_scores, num_keypoints, nonmaximum_supression_radius);
-figure(2);
-imshow(img_1);
-hold on;
-plot(keypoints_1(2, :), keypoints_1(1, :), 'rx', 'Linewidth', 2);
-hold off;
 
+if(debug_with_figures)
+    figure(2);
+    imshow(img_1);
+    hold on;
+    plot(keypoints_1(2, :), keypoints_1(1, :), 'rx', 'Linewidth', 2);
+    hold off;
+end
 %% Part 3 - Describe keypoints and show 16 strongest keypoint descriptors
 
 descriptors_1 = describeKeypoints(img_1, keypoints_1, descriptor_radius);
-figure(3);
-hold on;
-for i = 1:16
-    subplot(4, 4, i);
-    patch_size = 2 * descriptor_radius + 1;
-    imagesc(uint8(reshape(descriptors_1(:,i), [patch_size patch_size])));
-    axis equal;
-    axis off;
+
+if (debug_with_figures)
+    figure(3);
+    hold on;
+    for i = 1:16
+        subplot(4, 4, i);
+        patch_size = 2 * descriptor_radius + 1;
+        imagesc(uint8(reshape(descriptors_1(:,i), [patch_size patch_size])));
+        axis equal;
+        axis off;
+    end
+    hold off;
 end
-hold off;
 
 %% Part 4 - Match descriptors rsbetween first two images
 img_2 = img2;
@@ -57,29 +67,29 @@ descriptors_2 = describeKeypoints(img_2, keypoints_2, descriptor_radius);
 
 matches = matchDescriptors(descriptors_2, descriptors_1, match_lambda);
 
-
-
 [~, query_indices, match_indices] = find(matches);
 keypoints1 = keypoints_1(:, match_indices);
 keypoints2 = keypoints_2(:, query_indices);
 
-figure(4);
-imshow(img_1);
-hold on;
-title('First image');
-plot(keypoints_1(2, :), keypoints_1(1, :), 'rx', 'Linewidth', 2);
-plot(keypoints_1(2, match_indices), keypoints_1(1, match_indices), 'bx', 'Linewidth', 2);
-legend('Non-matched keypoints', 'Matched keypoints');
-hold off;
+if (debug_with_figures)
+    figure(4);
+    imshow(img_1);
+    hold on;
+    title('First image');
+    plot(keypoints_1(2, :), keypoints_1(1, :), 'rx', 'Linewidth', 2);
+    plot(keypoints_1(2, match_indices), keypoints_1(1, match_indices), 'bx', 'Linewidth', 2);
+    legend('Non-matched keypoints', 'Matched keypoints');
+    hold off;
 
-figure(5);
-imshow(img_2);
-hold on;
-title('Second image');
-plot(keypoints_2(2, :), keypoints_2(1, :), 'rx', 'Linewidth', 2);
-plot(keypoints_2(2, query_indices), keypoints_2(1, query_indices), 'bx', 'Linewidth', 2);
-legend('Non-matched keypoints','Matched keypoints');
-plotMatches(matches, keypoints_2, keypoints_1);
-hold off;
+    figure(5);
+    imshow(img_2);
+    hold on;
+    title('Second image');
+    plot(keypoints_2(2, :), keypoints_2(1, :), 'rx', 'Linewidth', 2);
+    plot(keypoints_2(2, query_indices), keypoints_2(1, query_indices), 'bx', 'Linewidth', 2);
+    legend('Non-matched keypoints','Matched keypoints');
+    plotMatches(matches, keypoints_2, keypoints_1);
+    hold off;
+end
 
 end

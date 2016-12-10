779a31147c5b1e308da44385ebdf7af7b9d2bbee
magrimm <marius.grimm93@web.de>
2016-12-10 14:32:45 +0100

    change number of keypoints

--- src/2d2d_correspondences/correspondences_2d2d.m
+++ src/2d2d_correspondences/correspondences_2d2d.m
@@ -1,10 +1,10 @@
-function matches = correspondences_2d2d(img0, img1)
+function [keypoints, keypoints_2, descriptors, descriptors_2, matches] = correspondences_2d2d(img0, img1)
 
 % Randomly chosen parameters that seem to work well - can you find better
 % ones?
 harris_patch_size = 9;
 harris_kappa = 0.08;
-num_keypoints = 200;
+num_keypoints = 1000;
 nonmaximum_supression_radius = 8;
 descriptor_radius = 9;
 match_lambda = 4;

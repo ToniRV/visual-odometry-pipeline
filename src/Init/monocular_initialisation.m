ef86b8ec48de28ef23381e733d9f9518027a37f0
magrimm <marius.grimm93@web.de>
2016-12-10 23:55:27 +0100

    move pose hypotheses to separate function

--- src/Init/monocular_initialisation.m
+++ src/Init/monocular_initialisation.m
@@ -61,35 +61,8 @@ function [state, T_cw] = moncular_initialisation(img0, img1, K)
     E = estimateEssentialMatrix(kp_fliped_homo_database, ...
         kp_fliped_homo_query, K, K)
 
-    % Decompose the matrix E by svd
-    [u,s,v]=svd(E);
-
-    % E = SR where S = [t]_x and R is the rotation matrix.
-    % E can be factorized as:
-    % s = u * z * u';
-    w = [0 -1 0; 1 0 0; 0 0 1];
-    z = [0 1 0; -1 0 0; 0 0 1];
-    
-    % Two possibilities:
-    rot1 = u * w  * v';
-    rot2 = u * w' * v';
-    % Two possibilities:
-    t1 = u(:,3) ./max(abs(u(:,3)));
-    t2 = -u(:,3) ./max(abs(u(:,3)));
-
-    % 4 possible choices of the camera matrix P2 based on the 2 possible
-    % choices of R and 2 possible signs of t.
-    rot(:,:,1) = rot1; 
-    t(:,:,1) = t1;
-
-    rot(:,:,2) = rot2; 
-    t(:,:,2) = t2;
-
-    rot(:,:,3) = rot1; 
-    t(:,:,3) = t2;
-
-    rot(:,:,4) = rot2; 
-    t(:,:,4) = t1;
+    % Get the hypotheses for the pose (rotation and translation)
+    [rot, t] = get_pose_hypotheses(E);
     
     %% Check if in front of camera
     % lambda0*[u v 1]^T = K1 * [Xw Yw Zw]^T

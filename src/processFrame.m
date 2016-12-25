3efbf766ee7c734f3ac681856aa5ee59e7ee719a
Toni <tonirosinol@hotmail.com>
2016-12-25 02:15:56 +0100

    Completely change processFrame.m

--- src/processFrame.m
+++ src/processFrame.m
@@ -2,110 +2,153 @@ function [ State_i1, Transform_i1, inlier_mask] = processFrame(Image_i1, Image_i
 %PROCESSFRAME Summary of this function goes here
 %   Detailed explanation goes here
 
-    % Run p3p+ransac to get transformation and new keypoints, both matched
+    %% Step 1 & 2:
+    % A) Run p3p+ransac to get transformation and new keypoints, both matched
     % and unmatched. Given the images and the correspondences 2D<->3D
     % correspondences and K.
-    [R_C_W, t_C_W, query_keypoints, all_matches, inlier_mask] = ...
+    [R_C_W, t_C_W, query_keypoints, query_descriptors, all_matches, inlier_mask] = ...
     ransacLocalization(Image_i1, Image_i0,  State_i0.keypoints_correspondences, ...
-    State_i0.p_W_landmarks_correspondences, State_i0.K);
+                                  State_i0.p_W_landmarks_correspondences, State_i0.K);
 
-    %Retrieve transformation
+    % B) Retrieve transformation
     Transform_i1 = [R_C_W, t_C_W];
+    inversed_transform_i1 = [R_C_W', -R_C_W'*t_C_W];
+    isLocalized = numel(R_C_W)>0;
 
-    % Get matched keypoints with both inliers and outliers
+    % Get matched keypoints and corresponding landmarks with both inliers and outliers
     matched_query_keypoints = query_keypoints(:, all_matches > 0);
-    % Get matched keypoints with only inliers
+    matched_corresponding_landmarks = State_i0.p_W_landmarks_correspondences(:, all_matches(all_matches > 0));
+    % Get matched keypoints and corresponding landmarks with only inliers
     inlier_matched_query_keypoints =  matched_query_keypoints(:, inlier_mask>0);
-    
-    
-    %% Step 3: trying to triang new landmarks
-    % a) Store the new non-matched query keypoints as candidates for
-    % triangulation in next iteration. We don't store keypoints that were
-    % matched and are outliers... should we?
-    non_matched_query_keypoints = query_keypoints(:, all_matches == 0);
-    State_i1.candidate_keypoints = non_matched_query_keypoints;
-    State_i1.candidates_transformation = Transform_i1;
-    
-    % These will be the new 2D-3D correspondences
-    points_3D = 0;
-    points_2D = 0;
-    % b) Try to find correspondences with previous candidates
-    if ((State_i0.candidate_keypoints ~= 0) &...
-            (State_i0.success == 1) & numel(R_C_W)>0) % we only do it if we already have candidates, and valid cameraMatrices, aka transforns
-    descriptor_radius = 9;
-    match_lambda = 4;
-    descriptors_i0_candidates = ...
-        describeKeypoints(Image_i0, State_i0.candidate_keypoints, descriptor_radius);
-    descriptors_i1_candidates = ...
-        describeKeypoints(Image_i1, State_i1.candidate_keypoints, descriptor_radius);
-    matches = matchDescriptors(descriptors_i1_candidates, descriptors_i0_candidates,...
-        match_lambda);
-    [~, query_indices, match_indices] = find(matches);
-    matched_i0_candidate_keypoints = State_i0.candidate_keypoints(:, match_indices);
-    matched_i1_candidate_keypoints = State_i1.candidate_keypoints(:, query_indices);
-    
-    % c) Try to find new 2D-3D correspondences
-        % TODO check that the bearing vector is bigger than a certain
-        % threshold
-        
-        % If ok, then triangulate
-        M_i0 = State_i0.candidates_transformation;
-        M_i1 = State_i1.candidates_transformation; % TODO check that this is correct, it actually depends on the Kitti coords.
-                                                                                    % According to the coords in this paper http://www.mrt.kit.edu/z/publ/download/2013/GeigerAl2013IJRR.pdf
-
-        % TODO can we actually use matlab's triangulate function?                                                                            
-        [new_X, reprojectionErrors] = ...
-            triangulate(flipud(matched_i1_candidate_keypoints)', flipud(matched_i0_candidate_keypoints)', M_i0', M_i1');
-        fprintf('It took %ds to compute matlab_triangulation \n', toc);
-        new_X = new_X';
-        reprojectionErrors = reprojectionErrors';
-        % TODO we should still remove 3d points with big reprojection error
-        % (i.e. >0.5?) 
-        reprojectionError_threshold = 0.10; % WARNING: this guy gets rid of MANY possible landmarks!
-        valid_errors = reprojectionErrors < reprojectionError_threshold;
-        valid_depth = new_X(3,:) > 0;
-        valid_indices = valid_errors & valid_depth;
-        
-        points_3D = new_X(:, valid_indices);
-        points_2D =  matched_i1_candidate_keypoints(:, valid_indices);
-    end
-    
-    %% 
-    % Get inlier matched landmarks
-    % all_matches gives the matches and non matches of the query keypoints,
-    % corresponding_matches gives the matched for only the ones that have been matched
-    % including outliers (which are excluded using inlier_mask)
-    corresponding_matches = all_matches(all_matches > 0);
-    matched_corresponding_landmarks = ...
-        State_i0.p_W_landmarks_correspondences(:, corresponding_matches);
     inlier_matched_corresponding_landmarks = matched_corresponding_landmarks(:, inlier_mask>0);
     
-    % Store only the matched inlier keypoints of the Image_i1 plus the new
-    % ones triangulated
-     if (isempty(points_2D) | points_2D ==0)
-         % WITH ONLY STORING INLIERS (!!this is the suggested method!!)
-%         State_i1.keypoints_correspondences = inlier_matched_query_keypoints;
-%         State_i1.p_W_landmarks_correspondences = inlier_matched_corresponding_landmarks;
-         % STORING ALL MATCHES
-        State_i1.keypoints_correspondences = matched_query_keypoints;
-        State_i1.p_W_landmarks_correspondences = matched_corresponding_landmarks;
-     else
-         %%% WITH ONLY STORING INLIERS (!!this is the suggested method!!)
-%            State_i1.keypoints_correspondences = [inlier_matched_query_keypoints, points_2D];
-        % Keep the found 3D landmarks that match to the inlier keypoints plus the new ones triangulated. TODO
-        % what about the rest of outlier landmarks, shouldn't we keep the
-        % previous 2d-3d correspondence for other frames? Probably yes, this
-        % way if there's an occlusion we can still track the landmark later...
-%             State_i1.p_W_landmarks_correspondences = [inlier_matched_corresponding_landmarks, points_3D];
-         %%% STORING ALL MATCHES
-          State_i1.keypoints_correspondences = [matched_query_keypoints, points_2D];
-          State_i1.p_W_landmarks_correspondences = [matched_corresponding_landmarks, points_3D];
-           end
-    State_i1.K = State_i0.K;
+    % C) Update State
+    keypoints_correspondences_i1 = inlier_matched_query_keypoints;
+    p_W_landmarks_correspondences_i1 = inlier_matched_corresponding_landmarks;
     
+    %% Step 3: trying to triangulate new landmarks
+    descriptor_radius = 9;
+    match_lambda = 4;
+    %First time we start:
+    if (isempty(State_i0.first_obs_candidate_keypoints))
+        new_first_obs_cand_kp_i1 = zeros(2, 0);
+        new_first_obs_cand_tf_i1 = zeros(12, 0);
+        new_last_obs_cand_kp_i1 = zeros(2, 0);
+        if(isLocalized)
+            new_first_obs_cand_kp_i1 = query_keypoints(:, all_matches == 0); % Non_matched_query_keypoints
+            % TODO find better way to deal with the transform of the first
+            % observed candidates
+            new_first_obs_cand_tf_i1 = repmat(reshape(Transform_i1,12,1), 1, sum(all_matches == 0)); % Store as 12xM transform
+            new_last_obs_cand_kp_i1 = new_first_obs_cand_kp_i1; %First time we store them we also fill last_obs as first_obs for uniformity
+        else
+            fprintf('[INFO] Iteration %d: set of first_observed_candidate_keypoints is empty and we have not localized \n', i1);
+        end
+        
+        % F) FINAL RESULT
+        State_i1.first_obs_candidate_keypoints = new_first_obs_cand_kp_i1;
+        State_i1.first_obs_candidate_transform = new_first_obs_cand_tf_i1;
+        State_i1.last_obs_candidate_keypoints = new_last_obs_cand_kp_i1;
+    else
+        %Subsequent times:
+        % A) Store input for this part:
+        first_obs_cand_kp_i0 = State_i0.first_obs_candidate_keypoints; 
+        first_obs_cand_tf_i0 = State_i0.first_obs_candidate_transform;
 
+        last_obs_cand_kp_i0 = State_i0.last_obs_candidate_keypoints;
+        last_obs_cand_descriptors_i0 = describeKeypoints(Image_i0, last_obs_cand_kp_i0, descriptor_radius);
+
+        non_matched_query_keypoints = query_keypoints(:, all_matches == 0);
+        non_matched_query_descriptors = query_descriptors(:, all_matches ==0);   
+
+        % B) Try to match all last_obs_cand_kp_i0 with current
+        % non_matched_query_keypoints
+        matches = matchDescriptors(non_matched_query_descriptors, last_obs_cand_descriptors_i0,...
+            match_lambda);
+        [~, query_indices, match_indices] = find(matches);
+
+        % C) If successful match, update track with new last obs kp, if unsuccessful
+        % discard track, aka delete last and first observed candidate kp together with transform.
+        last_obs_cand_kp_i1 = non_matched_query_keypoints(:, query_indices);
+        first_obs_cand_kp_i1 = first_obs_cand_kp_i0(:, match_indices);
+        first_obs_cand_tf_i1 = first_obs_cand_tf_i0(:, match_indices);
+
+        %If current pose is ok continue, if not do nothing.
+        if (isLocalized)
+            % D) For all the updated last_obs_cand_kp_i1 and corresponding
+            % first kp and tf do:
+            %%% I) Check which are suitable to triangulate:
+            is_triangulable = true(1, size(last_obs_cand_kp_i1, 2));
+            triangulable_last_kp = last_obs_cand_kp_i1(:, is_triangulable);
+            triangulable_last_tf = Transform_i1;
+            triangulable_first_kp = first_obs_cand_kp_i1(:, is_triangulable);
+            triangulable_first_tf = first_obs_cand_tf_i1(:, is_triangulable);
+
+            %%% II) Triangulate
+            % TODO can we actually use matlab's triangulate function?
+            % A way to optimize the following would be to vectorize over sets with common
+            % triangulable_first_tf...
+            % These will be the new 2D-3D correspondences
+            num_triang_kps = size(triangulable_last_kp, 2);
+            X_s = zeros(3, num_triang_kps);
+            list_reprojection_errors = zeros(1, num_triang_kps);
+            for i = 1:num_triang_kps
+                [newX_cam_frame, reprojectionError] = ...
+                triangulate(flipud(triangulable_last_kp(:, i))', flipud(triangulable_first_kp(:, i))',...
+                                  triangulable_last_tf', reshape(triangulable_first_tf(:,i), 3, 4)');
+                newX_cam_frame = newX_cam_frame';
+                X_s(:, i) = newX_cam_frame;
+                list_reprojection_errors(i) = reprojectionError;
+            end
+
+            %%% III) Update state
+            %%%% a) Store new 2D-3D correspondences which are valid
+            reprojectionError_threshold = 0.1; % WARNING: this guy gets rid of MANY possible landmarks!
+            valid_errors = list_reprojection_errors < reprojectionError_threshold;
+            valid_depth = X_s(3,:) > 0;
+            valid_indices = valid_errors & valid_depth;
+            points_3D_cam_frame = X_s(:, valid_indices);
+            % IS THIS CORRECT?
+            points_3D_W = inversed_transform_i1 * [points_3D_cam_frame; ones(1, nnz(valid_indices))];
+            points_2D = triangulable_last_kp(:, valid_indices);
+            
+            %%%% b) Append to already known 2D-3D correspondences
+            keypoints_correspondences_i1 = [keypoints_correspondences_i1,  points_2D];
+            p_W_landmarks_correspondences_i1 = [p_W_landmarks_correspondences_i1, points_3D_W];
+            
+            %%%% b) Clear triangulated tracks, both correctly and incorrectly
+            %%%% triangulated
+            last_obs_cand_kp_i1 = last_obs_cand_kp_i1(:, is_triangulable == 0);
+            first_obs_cand_kp_i1 = first_obs_cand_kp_i1(:, is_triangulable == 0);
+            first_obs_cand_tf_i1 = first_obs_cand_tf_i1(:, is_triangulable == 0);
+                
+
+            % E) For the non_matched_query_keypoints, append them as new candidates to current candidates.
+            %%% I) Create values:
+            new_first_obs_cand_kp_i1 = non_matched_query_keypoints(:, query_indices == 0); % These guys haven't been matched twice.
+            new_first_obs_cand_tf_i1 = repmat(reshape(Transform_i1, 12, 1), 1, size(new_first_obs_cand_kp_i1, 2));
+            new_last_obs_cand_kp_i1 = new_first_obs_cand_kp_i1; %First time we store them we also fill last_obs as first_obs for uniformity
+
+            %%% II) Append Results
+            last_obs_cand_kp_i1 = [last_obs_cand_kp_i1, new_last_obs_cand_kp_i1];
+            first_obs_cand_kp_i1 = [first_obs_cand_kp_i1, new_first_obs_cand_kp_i1];
+            first_obs_cand_tf_i1 = [first_obs_cand_tf_i1, new_first_obs_cand_tf_i1];
+
+            % F) FINAL RESULT
+            State_i1.last_obs_candidate_keypoints = last_obs_cand_kp_i1;
+            State_i1.first_obs_candidate_keypoints = first_obs_cand_kp_i1;
+            State_i1.first_obs_candidate_transform = first_obs_cand_tf_i1;
+        end
+    end % end of if (isempty(State_i0.first_obs_candidate_keypoints))
+    
+    % F) FINAL RESULTS
+    State_i1.keypoints_correspondences = keypoints_correspondences_i1;
+    State_i1.p_W_landmarks_correspondences = p_W_landmarks_correspondences_i1;
+
+    State_i1.K = State_i0.K;
+      
 %% Plotting
 % Distinguish success from failure.
+    corresponding_matches = all_matches(all_matches > 0);
     if (numel(R_C_W) > 0)
         subplot(1, 3, 3);
         num_inliers(i1) = nnz(inlier_mask);
@@ -138,4 +181,3 @@ function [ State_i1, Transform_i1, inlier_mask] = processFrame(Image_i1, Image_i
     pause(0.01);
 
 end
-

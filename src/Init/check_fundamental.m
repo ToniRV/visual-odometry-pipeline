fe70be0bc66fe2076581fc1d1d953f6a4c52c500
magrimm <marius.grimm93@web.de>
2017-01-02 16:51:20 +0100

    simplify check_fundamental

--- src/Init/check_fundamental.m
+++ src/Init/check_fundamental.m
@@ -1,49 +1,21 @@
-function [score, inliers] = check_fundamental(kp_database, kp_query, ...
-    F, num_matches, sigma)
+function [inliers] = check_fundamental(kp_database, kp_query, F, ...
+    num_matches)
 
-    th = 3.841;
-    th_score = 5.991;
-    inv_sigma_squared = 1.0/(sigma^2);
-    score = 0.0;
-    
+    % Initialize output variables
     inliers = false(num_matches, 1);
     
-    for i=1:num_matches
-        bool_inlier = true;
-        
+    for i=1:num_matches        
         kp1 = kp_database(:,i);
         kp2 = kp_query(:,i);
+
+        % Epipolar-Line-to-point Distances
+        square_dist_temp2= distPoint2EpipolarLine(F, kp1, kp2);
         
-        % Reprojection error in second image
-        % l2 = F * x1
-        l2 = F*kp1;
-        num2 = dot(l2, kp2);
-        square_dist1 = num2^2/(l2(1)^2+l2(2)^2);
-        chi_square1 = square_dist1*inv_sigma_squared;
-        
-        if (chi_square1 > th)
-           bool_inlier = false; 
-        else
-            score = score + th_score - chi_square1;
-        end
-        
-        % Reprojection error in second image
-        % l2 = F * x1
-        l1 = kp2'*F;
-        num1 = dot(l1, kp1);
-        square_dist2 = num1^2/(l1(1)^2+l1(2)^2);
-        chi_square2 = square_dist2*inv_sigma_squared;
-        
-        if (chi_square2 > th)
-           bool_inlier = false; 
-        else
-            score = score + th_score - chi_square2;
-        end
-        
-        if (bool_inlier)
-            inliers(i) = true;
-        else
+        % If error to epipolar line is to big --> outlier
+        if (square_dist_temp2 > 1)            
             inliers(i) = false;
+        else
+            inliers(i) = true;
         end
     end
 end
--- src/Init/ransac.m
+++ src/Init/ransac.m
@@ -1,4 +1,4 @@
-function [inlier_mask, F] = ransac(kp_database, kp_query, K, sigma)
+function [inlier_mask, F] = ransac(kp_database, kp_query, sigma)
     % Parameters
     num_iterations = 1000;
     num_matches = size(kp_database,2);
@@ -7,21 +7,21 @@ function [inlier_mask, F] = ransac(kp_database, kp_query, K, sigma)
     % 1) Perform RANAC iterations and save solution with hightest score
     for i=1:num_iterations
         % Select a minimum set
-        [p1i, idx] = datasample(kp_database, 8, 2, ...
-            'Replace', false);
+        [p1i, idx] = datasample(kp_database, 8, 2, 'Replace', false);
         p2i = kp_query(:,idx);
         
         % 2) Compute the Fundamental matrix
         Fi = fundamentalEightPoint_normalized(p1i, p2i);
+        
+        % 3) Current inliers
+        [current_inliers] = check_fundamental(kp_database, kp_query, ...
+            Fi, num_matches);
 
-        % 3) Current score and inliers
-        [current_score, current_inliers] = check_fundamental(kp_database, ...
-            kp_query, Fi, num_matches, sigma);
-    
-        if (current_score > top_score)
+        % 4) Choose Fundamental matrix with most inliers
+        if (nnz(current_inliers) > top_score)
            F = Fi;
            inlier_mask = current_inliers;
-           top_score = current_score;
+           top_score = nnz(current_inliers);
         end
     end
 end

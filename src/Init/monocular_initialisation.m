3eb8c7c95f92972f3509fcab471076d8152f85c6
magrimm <marius.grimm93@web.de>
2017-01-04 20:35:31 +0100

    flipped keypoints for ex. 5 functions; changed plotting; --> completely working now --> improve time

--- src/Init/monocular_initialisation.m
+++ src/Init/monocular_initialisation.m
@@ -54,21 +54,77 @@ function [state, T_cw] = monocular_initialisation(img0, img1, K)
     kp_homo_query = ...
         [kp_matches_query; ones(1, size(kp_matches_query, 2))];
     
+    %% Flip keypoints for right input of functions from exercise 5
+    kp_homo_database_fl = kp_homo_database([2 1 3], :);
+    kp_homo_query_fl = kp_homo_query([2 1 3], :);
+    
     %% Find fundamental matrix
     tic;
-    [inlier_mask, F] = ransac(kp_homo_database, kp_homo_query);
+    [inlier_mask, F] = ransac(kp_homo_database_fl, kp_homo_query_fl);
     inliers_F = nnz(inlier_mask)
     sprintf('Time needed: Find fundamental matrix: %f seconds', toc)
     
     %% Obtain inliers from inlier_mask
     kp_homo_database = kp_homo_database(:, inlier_mask);
     kp_homo_query = kp_homo_query(:, inlier_mask);
-        
-    %% Estimate Essential matrix
-    E = estimateEssentialMatrix(kp_homo_database, kp_homo_query, K, K);
-    E = K' * F * K;
+    kp_homo_database_fl = kp_homo_database_fl(:, inlier_mask);
+    kp_homo_query_fl = kp_homo_query_fl(:, inlier_mask);    
     
-    %% Plot matching features
+    %% Estimate Essential matrix
+    tic;
+    E = estimateEssentialMatrix(kp_homo_database_fl, kp_homo_query_fl, K, K)
+    sprintf('Time needed: exercise estimateEssentialMatrix: %f seconds', toc)
+    
+    if (debug_verbose)
+        tic;
+        F_test = estimateFundamentalMatrix(kp_homo_database_fl(1:2,:)', ...
+            kp_homo_query_fl(1:2,:)')
+        E_test = K' * F_test * K
+        sprintf('Time needed: matlab estimateFundamentalMatrix: %f seconds', toc)
+    end
+    
+    %% Get the hypotheses for the pose (rotation and translation)
+    %[rot, u3] = get_pose_hypotheses(E)
+    [R_hypo, u3]= decomposeEssentialMatrix(E);
+
+    %% Check if in front of camera
+    % lambda0*[u v 1]^T = K1 * [Xw Yw Zw]^T
+    % lambda1*[u v 1]^T = K1 * R1* [Xw Yw Zw]^T + T
+    [R, T, P, M1, M2] = disambiguateRelativePose(R_hypo, u3, ...
+        kp_homo_database_fl, kp_homo_query_fl, K, K);
+    
+    %% ONLY TESTING
+    % Rescale reprojection to homogenous coordinates again (u v 1)
+    p_homo_database = zeros(3, size(P, 2));
+    p_homo_query = zeros(3, size(P, 2));
+    for i=1:size(P,2)
+        p_homo_database(:,i) = M1*P(:,i);
+        p_homo_database(:,i) = p_homo_database(:,i) ./p_homo_database(3,i);
+        p_homo_query(:,i) = M2*P(:,i);
+        p_homo_query(:,i) = p_homo_query(:,i) ./p_homo_query(3,i);
+    end
+
+    % Reprojection error
+    % First image
+    difference_db = kp_homo_database_fl - p_homo_database;
+    errors_db = sum(difference_db.^2, 1);
+    errordb = sum(sqrt(errors_db))/sqrt(size(P,2));
+    % Second image
+    difference_qu = kp_homo_query_fl - p_homo_query;
+    errors_qu = sum(difference_qu.^2, 1);
+    errorqu = sum(sqrt(errors_qu))/sqrt(size(P,2));
+        
+    % Check the epipolar constraint x2(i).' * F * x1(i) = 0 for all points i.
+    N = size(kp_homo_query_fl,2);
+    cost_algebraic = norm( sum(kp_homo_query_fl.*(F*kp_homo_database_fl)) ) / sqrt(N);
+    cost_dist_epi_line = distPoint2EpipolarLine(F,kp_homo_database_fl, ...
+        kp_homo_query_fl);
+    
+    reprojection_errors = [errordb, errorqu]
+    costs = [cost_algebraic, cost_dist_epi_line]
+    
+    %% PLOT
+    % Plot matching features
     if (debug_verbose)
         figure(5); 
         subplot(2,1,1);
@@ -77,45 +133,11 @@ function [state, T_cw] = monocular_initialisation(img0, img1, K)
         subplot(2,1,2);
         showMatchedFeatures(img0, img1, flipud(kp_homo_database(1:2,:))', ...
             flipud(kp_homo_query(1:2,:))', 'montage');
+        
+        % Plot 3d scene (trajectory and landmarks)
+        plot_trajectory_with_landmarks(img0, img1, kp_homo_database_fl, ...
+            kp_homo_query_fl, P, R, T);
     end
-
-    %% Get the hypotheses for the pose (rotation and translation)
-    %[rot, u3] = get_pose_hypotheses(E)
-    [R, u3]= decomposeEssentialMatrix(E);
-
-    %% Check if in front of camera
-    % lambda0*[u v 1]^T = K1 * [Xw Yw Zw]^T
-    % lambda1*[u v 1]^T = K1 * R1* [Xw Yw Zw]^T + T
-    [R, T, P, M1, M2] = disambiguateRelativePose(R, u3, ...
-        kp_homo_database, kp_homo_query, K, K);
-
-    %% ONLY TESTING
-    % Rescale reprojection to homogenous coordinates again (u v 1)
-    for i=1:size(P,2)
-        p_homo(:,i) = M1*P(:,i);
-        p_homo(:,i) = p_homo(:,i) ./p_homo(3,i);
-    end
-    
-    for i=1:size(P,2)
-        p_homo2(:,i) = M2*P(:,i);
-        p_homo2(:,i) = p_homo2(:,i) ./p_homo2(3,i);
-    end
-    
-    % Reprojection error
-    % First image
-    difference_db = kp_homo_database - p_homo;
-    errors_db = sum(difference_db.^2, 1);
-    errordb = sum(sqrt(errors_db))/sqrt(size(P,2))
-    % Second image
-    difference_qu = kp_homo_query - p_homo2;
-    errors_qu = sum(difference_qu.^2, 1);
-    errorqu = sum(sqrt(errors_qu))/sqrt(size(P,2))
-    
-    % Check the epipolar constraint x2(i).' * F * x1(i) = 0 for all points i.
-    N = size(kp_homo_query,2);
-    cost_algebraic = norm( sum(kp_homo_query.*(F*kp_homo_database)) ) / sqrt(N)
-    cost_dist_epi_line = distPoint2EpipolarLine(F,kp_homo_database, ...
-        kp_homo_query)
     
     %% OUTPUT
     % State

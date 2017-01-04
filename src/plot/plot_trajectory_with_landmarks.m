c8087d4f4ca6560194726c0e4adead3db15d7780
magrimm <marius.grimm93@web.de>
2017-01-05 00:06:46 +0100

    fix plotting of frames

--- src/plot/plot_trajectory_with_landmarks.m
+++ src/plot/plot_trajectory_with_landmarks.m
@@ -13,13 +13,16 @@ function plot_trajectory_with_landmarks(img0, img1, kp_homo_database, ...
     hold on
     plot(kp_homo_query(1,:), kp_homo_query(2,:), 'ys');
     title('Image 2')
+    
     % Display camera pose
-    plotCoordinateFrame(eye(3),zeros(3,1), 20);
-    %text(-0.1,-0.1,-0.1,'Cam 1','fontsize',10,'color','k','FontWeight','bold');
-
+    % First coodinate frame at origin
     subplot(2,3,[2,3,5,6])
+    plotCoordinateFrame(eye(3),zeros(3,1), 10);
+    %text(-0.1,-0.1,-0.1,'Cam 1','fontsize',10,'color','k','FontWeight','bold');
+    
+    % Second coordinate frame
     center_cam2_W = -R'*T;
-    plotCoordinateFrame(R',center_cam2_W, 20);
+    plotCoordinateFrame(R',center_cam2_W, 10);
     %text(center_cam2_W(1)-0.1, center_cam2_W(2)-0.1, center_cam2_W(3)-0.1,'Cam 2','fontsize',10,'color','k','FontWeight','bold');
     
     % Landmarks

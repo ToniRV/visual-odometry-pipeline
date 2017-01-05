e274b6c3b572bc7861c39c6290ebb490702c7323
magrimm <marius.grimm93@web.de>
2017-01-05 23:17:19 +0100

    clean up changed input output

--- src/Init/auto_init_frames.m
+++ src/Init/auto_init_frames.m
@@ -1,5 +1,5 @@
 function [img0, img1, kp_homo_initial_matched, state, T_cw, ...
-    descriptors_current_matched, reprojection_errors, costs] = ...
+    reprojection_errors, costs] = ...
     auto_init_frames(initial_frame, current_frame, K)
 
     % Verbose output for debug
@@ -13,26 +13,19 @@ function [img0, img1, kp_homo_initial_matched, state, T_cw, ...
     %% Keypoint Detection & Matching
     % Detect keypoints in both frames, obtain descriptors and find matches
     tic;
-    [keypoints_initial_frame, keypoints_current_frame, ~, ...
-        descriptors_current, matches] = ...
-        correspondences_2d2d(initial_frame, current_frame, N);
+    [kp_homo_initial_matched, keypoints_current_frame] = ...
+        correspondences_2d2d_auto_frames(initial_frame, current_frame, N);
     sprintf('Time needed: correspondences_2d2d: %f seconds', toc)
         
-    %% Extraction of matching keypoints
-    [~, query_indices, match_indices] = find(matches);
-    kp_initial_matched = keypoints_initial_frame(:, match_indices);
-    kp_current_matched = keypoints_current_frame(:, query_indices);
-    descriptors_current_matched = descriptors_current(:, query_indices);
-    
-    %% Homogenous fliped keypoint coordinates
-    kp_homo_initial_matched = ...
-        [kp_initial_matched; ones(1, size(kp_initial_matched, 2))];
-    kp_homo_current_matched = ...
-        [kp_current_matched; ones(1, size(kp_current_matched, 2))];
-    
     %% Flip keypoints for right input of functions from exercise 5
     kp_homo_database_fl = kp_homo_initial_matched([2 1 3], :);
-    kp_homo_query_fl = kp_homo_current_matched([2 1 3], :);
+    kp_homo_query_fl = keypoints_current_frame([2 1 3], :);
+    
+    % For later plotting
+    if (debug_verbose)
+        kp1h_matched_before_ransac = kp_homo_initial_matched;
+        kp2h_matched_before_ransac = keypoints_current_frame;
+    end
     
     %% Find fundamental matrix
     tic;
@@ -42,7 +35,7 @@ function [img0, img1, kp_homo_initial_matched, state, T_cw, ...
     
     %% Obtain inliers from inlier_mask
     kp_homo_initial_matched = kp_homo_initial_matched(:, inlier_mask);
-    kp_homo_current_matched = kp_homo_current_matched(:, inlier_mask);
+    kp_homo_current_matched = keypoints_current_frame(:, inlier_mask);
     kp_homo_database_fl = kp_homo_database_fl(:, inlier_mask);
     kp_homo_query_fl = kp_homo_query_fl(:, inlier_mask);    
     
@@ -103,15 +96,15 @@ function [img0, img1, kp_homo_initial_matched, state, T_cw, ...
     if (debug_verbose)
         figure(5); 
         subplot(2,1,1);
-        showMatchedFeatures(initial_frame, current_frame, flipud(kp_initial_matched)', ...
-            flipud(kp_current_matched)', 'montage');       
+        showMatchedFeatures(initial_frame, current_frame, flipud(kp1h_matched_before_ransac(1:2,:))', ...
+            flipud(kp2h_matched_before_ransac(1:2,:))', 'montage');       
         subplot(2,1,2);
         showMatchedFeatures(initial_frame, current_frame, flipud(kp_homo_initial_matched(1:2,:))', ...
             flipud(kp_homo_current_matched(1:2,:))', 'montage');
         
         % Plot 3d scene (trajectory and landmarks)
-        plot_trajectory_with_landmarks(initial_frame, current_frame, kp_homo_database_fl, ...
-            kp_homo_query_fl, P, R, T);
+%         plot_trajectory_with_landmarks(initial_frame, current_frame, kp_homo_database_fl, ...
+%             kp_homo_query_fl, P, R, T);
     end
     
     %% OUTPUT

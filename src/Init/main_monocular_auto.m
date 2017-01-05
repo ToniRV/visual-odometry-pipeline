1361c5ff6a98e9ad221d07df357400bae4dab614
magrimm <marius.grimm93@web.de>
2017-01-06 00:25:58 +0100

    flag for auto_frame_initialisation or single monocular

--- src/Init/main_monocular_auto.m
+++ src/Init/main_monocular_auto.m
@@ -42,78 +42,116 @@ else
     assert(false);
 end
 
-%% Bootstrap
-% need to set bootstrap_frames
-if ds == 0
-    initial_frame = imread([kitti_path '/00/image_0/' ...
-        sprintf('%06d.png',0)]);
-elseif ds == 1
-    initial_frame = rgb2gray(imread([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-        left_images(0).name]));
-elseif ds == 2
-    initial_frame = rgb2gray(imread([parking_path ...
-        sprintf('/images/img_%05d.png',0)]));
-else
-    assert(false);
-end
-    
-max_num_auto_frames = 15;
-min_num_inliers = 30;
-inliers = zeros(max_num_auto_frames, 1);
-errors = zeros(max_num_auto_frames, 2);
-costs = zeros(max_num_auto_frames, 2);
-for i = 1:max_num_auto_frames
+%% Initialisation
+
+is_auto_frame_monocular_initialisation = true;
+
+if (is_auto_frame_monocular_initialisation) 
+    % Automatically choose the best initialisation frames
     if ds == 0
-        current_frame = imread([kitti_path '/00/image_0/' ...
-            sprintf('%06d.png',i)]);
+        initial_frame = imread([kitti_path '/00/image_0/' ...
+            sprintf('%06d.png',0)]);
     elseif ds == 1
-        current_frame = rgb2gray(imread([malaga_path ...
+        initial_frame = rgb2gray(imread([malaga_path ...
             '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-            left_images(i).name]));
+            left_images(0).name]));
     elseif ds == 2
-        current_frame = rgb2gray(imread([parking_path ...
-            sprintf('/images/img_%05d.png',i)]));
+        initial_frame = rgb2gray(imread([parking_path ...
+            sprintf('/images/img_%05d.png',0)]));
     else
         assert(false);
     end
-    
-    % Automatically choosing frames
-    [img0, img1, kp_database, state, T_cw, ...
-        reprojection_errors, dist_epi_costs] = auto_init_frames(...
-        initial_frame, current_frame, K);
-    
-    if (size(state.matches_2d, 2) < min_num_inliers)
-        break;
-    end
-    
-    inliers(i,:) = size(state.matches_2d, 2);
-    errors(i,:) = reprojection_errors;
-    costs(i,:) = dist_epi_costs;
-end
 
-smallest_error = Inf;
-for i = 1:nnz(inliers)
-    error = sum(errors(i,:))/2;
-    
-    if (error < smallest_error)
-        smallest_error = error;
-        
+    max_num_auto_frames = 15;
+    min_num_inliers = 30;
+    Transformations_cw = zeros(4, 4, max_num_auto_frames);
+    inliers = zeros(max_num_auto_frames, 1);
+    errors = zeros(max_num_auto_frames, 2);
+    costs = zeros(max_num_auto_frames, 2);
+    for i = 1:max_num_auto_frames
         if ds == 0
-            img1 = imread([kitti_path '/00/image_0/' ...
+            current_frame = imread([kitti_path '/00/image_0/' ...
                 sprintf('%06d.png',i)]);
         elseif ds == 1
-            img1 = rgb2gray(imread([malaga_path ...
+            current_frame = rgb2gray(imread([malaga_path ...
                 '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
                 left_images(i).name]));
         elseif ds == 2
-            img1 = rgb2gray(imread([parking_path ...
+            current_frame = rgb2gray(imread([parking_path ...
                 sprintf('/images/img_%05d.png',i)]));
         else
             assert(false);
         end
+
+        % Automatically choosing frames
+        [state_i, T_cw_i, reprojection_errors, dist_epi_costs] = ...
+            auto_init_frames(initial_frame, current_frame, K);
+
+        if (size(state_i.matches_2d, 2) < min_num_inliers)
+            break;
+        end
+
+        states(i,:) = state_i;
+        Transformations_cw(:,:, i) = T_cw_i; 
+        inliers(i,:) = size(state_i.matches_2d, 2);
+        errors(i,:) = reprojection_errors;
+        costs(i,:) = dist_epi_costs;
     end
-end
+
+    smallest_error = Inf;
+    for i = 1:nnz(inliers)
+        error = sum(errors(i,:))/2;
+
+        if (error < smallest_error)
+            smallest_error = error;
+
+            if ds == 0
+                img1 = imread([kitti_path '/00/image_0/' ...
+                    sprintf('%06d.png',i)]);
+            elseif ds == 1
+                img1 = rgb2gray(imread([malaga_path ...
+                    '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+                    left_images(i).name]));
+            elseif ds == 2
+                img1 = rgb2gray(imread([parking_path ...
+                    sprintf('/images/img_%05d.png',i)]));
+            else
+                assert(false);
+            end
+
+            state = states(i, :);
+            T_cw = Transformations_cw(:, :, i);
+        end
+    end
+else
+    %% Bootstrap
+    % Need to set bootstrap_frames
+    if ds == 0
+        bootstrap_frames = [1 3]; % Frames suggested in the project description
+        img0 = imread([kitti_path '/00/image_0/' ...
+            sprintf('%06d.png',bootstrap_frames(1))]);
+        img1 = imread([kitti_path '/00/image_0/' ...
+            sprintf('%06d.png',bootstrap_frames(2))]);
+
+    elseif ds == 1
+        bootstrap_frames = [0 2];
+        img0 = rgb2gray(imread([malaga_path ...
+            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+            left_images(bootstrap_frames(1)).name]));
+        img1 = rgb2gray(imread([malaga_path ...
+            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+            left_images(bootstrap_frames(2)).name]));
+
+    elseif ds == 2
+        bootstrap_frames = [0 2]; % [1 3]
+        img0 = rgb2gray(imread([parking_path ...
+            sprintf('/images/img_%05d.png',bootstrap_frames(1))]));
+        img1 = rgb2gray(imread([parking_path ...
+            sprintf('/images/img_%05d.png',bootstrap_frames(2))]));
+    else
+        assert(false);
+    end
     
-%% Monocular initialisation
-%[state, T_cw] = monocular_initialisation(img0, img1, K);
+    % Monocular initialisation
+    [state, T_cw] = monocular_initialisation(img0, img1, K); 
+end

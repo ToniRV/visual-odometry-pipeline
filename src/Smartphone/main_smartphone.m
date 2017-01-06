cab26780f2a744f94c7d0cdd0af296251675af44
Toni <tonirosinol@hotmail.com>
2017-01-06 23:49:06 +0100

    Add main_smartphone.m

---
+++ src/Smartphone/main_smartphone.m
@@ -0,0 +1,32 @@
+debug = true;
+
+parameters = struct(...
+    'debug', debug...
+    );
+
+% Get camera parameters
+cameraParams = calibrateSmartphone(parameters);
+
+% Create video writer
+WriteFilename = 'undistorted_video.mov';
+videoFWriter = vision.VideoFileWriter(WriteFilename, 'FileFormat', 'AVI', 'FrameRate',videoFReader.info.VideoFrameRate);
+
+% Create video reader
+ReadFilename = 'video.mov';
+videoFReader = vision.VideoFileReader(ReadFilename, 'ImageColorSpace', Intensity);
+
+% Create video player
+videoPlayer = vision.VideoPlayer;
+
+% Loop over frames
+while ~isDone(videoFReader)
+    I = step(videoFReader);
+    undistorted_I = undistortImage(I, cameraParams);
+    step(videoFWriter, undistorted_I);
+    if (debug)
+        step(videoPlayer, videoFrame);
+    end
+end
+release(videoPlayer);
+release(videoFReader);
+release(videoFWriter);

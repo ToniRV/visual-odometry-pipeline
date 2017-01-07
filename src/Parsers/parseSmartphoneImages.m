a7d08e25d2c53f3f2efad15efa872c18b24ae79e
Toni <tonirosinol@hotmail.com>
2017-01-07 17:57:06 +0100

    Change smartphone parser with rgb2gray

--- src/Parsers/parseSmartphoneImages.m
+++ src/Parsers/parseSmartphoneImages.m
@@ -2,5 +2,5 @@ function [img] = parseSmartphoneImages(dataset_path, index)
     %Load images
     imageNames = dir(fullfile(dataset_path,'*.jpg'));
     imageNames = {imageNames.name}';
-    img = imread(fullfile(dataset_path,imageNames{index}));
+    img = rgb2gray(imread(fullfile(dataset_path,imageNames{index})));
 end

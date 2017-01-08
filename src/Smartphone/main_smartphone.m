abb7d42266dfc17c3bfa38bef7b69759a43735be
Toni <tonirosinol@hotmail.com>
2017-01-08 12:02:50 +0100

    Make smartphone work with paths and all

--- src/Smartphone/main_smartphone.m
+++ src/Smartphone/main_smartphone.m
@@ -3,23 +3,20 @@ close all;
 clc
 
 % Path to dataset
-dataset_path = '/home/tonirv/Downloads/smartphone_dataset/';
+dataset_path = '/home/tonirv/Downloads/frame_set_3/';
 debug = false;
 
 load('/home/tonirv/Documents/visual-odometry-pipeline/src/Smartphone/data/cameraParams.mat');
-if(isempty(dataset_path))
-    % Get camera parameters
-    cameraParams = calibrateSmartphone;
-end
 
 %Load images
-imageNames = dir(fullfile(dataset_path,'a*.jpeg'));
+imageNames = dir(fullfile(dataset_path,'*.jpg'));
 imageNames = {imageNames.name}';
 
 % Loop over frames
 for i = 1:size(imageNames, 1)
     img_gray = rgb2gray(imread(fullfile(dataset_path,imageNames{i})));
-    img_undistorted = undistortImage(img_gray, cameraParams, 'OutputView', 'valid');
+    img_res = imresize(img_gray, 0.1);
+    img_undistorted = undistortImage(img_res, cameraParams, 'OutputView', 'valid');
     if (debug)
         figure();
         subplot(1, 2, 1);

efdeb4f037a7c33efe982e3be47fc0fdb0bce82a
Toni <tonirosinol@hotmail.com>
2017-01-08 12:06:00 +0100

    Update rename_script.sh

--- src/Smartphone/rename_script.sh
+++ src/Smartphone/rename_script.sh
@@ -4,8 +4,9 @@ i=0
 for f in $FILES
 do
   i=$(($i+1))
+  filename=$(printf "%04d" $i)
   echo "Processing $f file..."
-  new_name=/home/tonirv/Downloads/dataset/$i.jpg
-  mv "$f" "$new_name"
+  new_name=/home/tonirv/Downloads/dataset3/$filename.jpg
+  cp "$f" "$new_name"
   # take action on each file. $f store current file name
 done
